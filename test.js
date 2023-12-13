e08165cb04329d6b584a03e4027ed973e9dd7ce3
belanovic <goranbelanovic@yahoo.com>
2023-12-13 14:28:47 +0100

    msg

--- test.js
+++ test.js
@@ -8,6 +8,8 @@ const bcrypt = require('bcrypt');
 const _ = require('lodash');
 const Joi = require('joi');
 const config = require('config');
+const cookieParser = require('cookie-parser');
+
 
 server.listen(2000, 'localhost', () => {console.log("Server is listening on port 2000")});
 
@@ -15,7 +17,7 @@ const mongoAddress = `mongodb+srv://goran:1234@cluster0.pg5bd.mongodb.net/?retry
 
 mongoose.connect(mongoAddress, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('Connected to the database'))
-    .catch(err => console.log(err))
+    .catch(error => console.log(error))
 
 const schema_film = new mongoose.Schema({
     title: String,
@@ -32,8 +34,8 @@ app.get('/film/:title/:year', async (req, res) => {
     try {
         const saved_film = await new_film.save();
         res.status(200).send(`Film ${saved_film.title} saved to database`)
-    }catch(err) {
-        res.send(err.message)
+    }catch (error) {
+        res.json({error: modify_error(error)})
     }
     
 })
@@ -42,6 +44,7 @@ app.use(express.json({
     type: ['application/json', 'text/plain'],
     limit: '50mb'
 }));
+app.use(cookieParser());
 
 app.use(function (req, res, next) {
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
@@ -51,16 +54,16 @@ app.use(function (req, res, next) {
     next()
 })
 
-app.get('/find', auth, async (req, res) => {
+app.get('/find', auth_cookie, async (req, res) => {
     try {
         const all_films = await Film.find()
         return res.json(all_films);
     } catch (error) {
-        res.json({error: error})
+        res.json({error: modify_error(error)})
     }
 
 })
-app.get('/find/:title', async (req, res) => {
+app.get('/find/:title', auth_cookie, async (req, res) => {
     try {
         const films = await Film.findOne({title: req.params.title})
         if(films) {
@@ -69,7 +72,7 @@ app.get('/find/:title', async (req, res) => {
             return res.json(`The movie with the title ${req.params.title} wasn't found`);
         }
     } catch(error) {
-        res.json({error: error})
+        res.json({error: modify_error(error)})
     }
 })
 
@@ -82,24 +85,37 @@ const User = mongoose.model('User', user_schema);
 
 
 app.post('/register', async (req, res) => {
+    let registration_msg = {
+        registered: '',
+        username: '',
+        failure_msg: ''
+    }
     try {
         let user_data = _.pick(req.body, ['username', 'password']);
         const {error} = validate_data(user_data);
-        if(error) return res.json(error.message);
-
+        if(error) {
+            registration_msg.registered = false;
+            registration_msg.failure_msg = error.message;
+            return res.json({registration_msg: registration_msg});
+        }
         const username_taken = await User.findOne({username: user_data.username});
-        if(username_taken) return res.json(`Username ${username_taken.username} is already taken`);
+        if(username_taken) {
+            registration_msg.registered = false;
+            registration_msg.failure_msg = `Username ${username_taken.username} is already taken`;
+            return res.json({registration_msg: registration_msg});
+        }
 
         const salt = await bcrypt.genSalt(5);
         const crypted_password = await bcrypt.hash(user_data.password, salt);
         user_data.password = crypted_password;
         
         const new_user = new User(user_data);
-        console.log(new_user);
-        new_user.save();
-        res.json(new_user);
+        const user_saved = await new_user.save();
+        registration_msg.registered = true;
+        registration_msg.username = user_saved.username;
+        return res.json({registration_msg: registration_msg});
     } catch (error) {
-        res.json(error.message);
+        return res.json({error: modify_error(error)});
     }
 })
 
@@ -112,58 +128,87 @@ function validate_data(user_data) {
 }
 
 app.post('/login', async (req, res) => {
-    console.log('evo meeeee');
-    let login_message = {
-        username: '',
+    let login_msg = {
         logged_in: '',
+        username: '',
         token: '',
-        error_message: ''
+        failure_msg: ''
     }
     try {
         const user_data = _.pick(req.body, ['username', 'password']);
         const {error} = validate_data(user_data);
 
         if(error) {
-            login_message.logged_in = false;
-            login_message.error_message = error.message
-            return res.json(login_message);
+            login_msg.logged_in = false;
+            login_msg.failure_msg = error.message
+            return res.json({login_msg: login_msg});
         }
 
         const user_registered = await User.findOne({username: user_data.username});
         if(!user_registered) {
-            login_message.logged_in = false;
-            login_message.error_message = `User with username ${user_data.username} doesn't exist`;
-            return res.json(login_message);
+            login_msg.logged_in = false;
+            login_msg.failure_msg = `User with username ${user_data.username} doesn't exist`;
+            return res.json({login_msg: login_msg});
         }
 
         const password_correct = await bcrypt.compare(user_data.password, user_registered.password);
         if(!password_correct) {
-            login_message.logged_in = false;
-            login_message.error_message = `Username or password are incorrect`;
-            return res.json(login_message);
+            login_msg.logged_in = false;
+            login_msg.failure_msg = `Username or password are incorrect`;
+            return res.json({login_msg: login_msg});
 
         }
         
-        const token = jwt.sign(user_data, config.get('jwtPrivateKey'))
-        login_message.logged_in = true;
-        login_message.username = user_data.username
-        login_message.token = token;
-        res.json(login_message);
+        const token = jwt.sign(user_data, config.get('jwtPrivateKey'), {expiresIn: '1h'});
+        login_msg.logged_in = true;
+        login_msg.username = user_data.username;
+        login_msg.token = token;
+        return res.cookie('token', token, {/* httpOnly: true, */}).json({login_msg: login_msg});
 
     } catch (error) {
-        login_message.logged_in = false;
-        login_message.rror_message = error.message;
-        return res.json(login_message);
+        return res.json({error: modify_error(error)});
     }
 })
 
-function auth(req, res, next) {
 
+function auth_cookie(req, res, next) {
+    try {
+        const token = req.cookies.token;
+        console.log(token);
+        const decoded_payload = jwt.verify(token, config.get('jwtPrivateKey'));
+        next()
+    } catch (error) {
+        return res.clearCookie('token').json({error: modify_error(error)});
+    }
+}
+
+/* function auth_header(req, res, next) {
     try {
         const token = req.headers.authorization.split(' ')[1];
         const decoded_payload = jwt.verify(token, config.get('jwtPrivateKey'));
         next()
     } catch (error) {
-        return res.json({error: error});
+        return res.json({error: modify_error(error)});
     }
+} */
+
+function modify_error(err) {
+    if(err.name =='MongooseError'
+    || err.name =='CastError'
+    || err.name =='DivergentArrayError'
+    || err.name =='MissingSchemaError'
+    || err.name =='DocumentNotFoundError'
+    || err.name =='ValidatorError'
+    || err.name =='ValidationError'
+    || err.name =='MissingSchemaError'
+    || err.name =='ObjectExpectedError'
+    || err.name =='ObjectParameterError'
+    || err.name =='OverwriteModelError'
+    || err.name =='ParallelSaveError'
+    || err.name =='StrictModeError'
+    || err.name =='VersionError') {
+        err.message = `Problem with the database. ${err.name}`;
+    }
+    const stringified_error = JSON.stringify(err, Object.getOwnPropertyNames(err));
+    return JSON.parse(stringified_error)
 }
